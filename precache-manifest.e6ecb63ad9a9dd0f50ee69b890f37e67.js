8b34da36fd2dd8a5accdb3b8b9da3fb3d89750ee
Karthick <karthickthankyou@gmail.com>
2020-06-07 11:47:37 +0530

    Updates

---
+++ precache-manifest.e6ecb63ad9a9dd0f50ee69b890f37e67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee217d9cd8d93a45494e5135e3268c78",
+    "url": "/karthickthankyou/redux-saga-tmdb/index.html"
+  },
+  {
+    "revision": "52ac1789b486fb4055c9",
+    "url": "/karthickthankyou/redux-saga-tmdb/static/css/main.ba8569ca.chunk.css"
+  },
+  {
+    "revision": "230e9ee043e6873643fc",
+    "url": "/karthickthankyou/redux-saga-tmdb/static/js/2.b38701c0.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/karthickthankyou/redux-saga-tmdb/static/js/2.b38701c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ac1789b486fb4055c9",
+    "url": "/karthickthankyou/redux-saga-tmdb/static/js/main.8b0075b7.chunk.js"
+  },
+  {
+    "revision": "5f287e9876533d85f130",
+    "url": "/karthickthankyou/redux-saga-tmdb/static/js/runtime-main.2cfc5607.js"
+  }
+]);
